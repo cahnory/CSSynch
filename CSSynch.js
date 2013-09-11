587492cb28a9f525fa42ad228d2a04d45a140b23
François Germain <cahnory@gmail.com>
2013-09-11 14:45:26 +0200

    First commit

---
+++ CSSynch.js
@@ -0,0 +1,46 @@
+/*jslint browser: true, indent: 2 */
+/*global jQuery */
+(function ($) {
+  'use strict';
+
+  // Refresh delay in milliseconds
+  var delay, refreshed, parameter;
+
+  delay     = 2000;
+  refreshed = 0;
+  parameter = 'cssRefresh';
+
+  $('link[rel="stylesheet"]').each(function () {
+    var uri, last, update;
+
+    // Original css is the last version
+    last = $(this);
+
+    // Uri, prepared to inject uniqid query parameter
+    uri = last.attr('href').replace(/\?|$/, '?&');
+
+    // Refresh only url of type: (//, http://, https://, no sheme)
+    if (uri.match(/^(?:https?:)?\/\/|^(?:.(?!\:\/\/))+$/)) {
+      update = function () {
+        refreshed++;
+  
+        // Create new version
+        $('<link rel="stylesheet"/>')
+          .insertAfter(last)
+          .on('load', function () {
+            // Remove previous version
+            last.remove();
+            last = $(this);
+  
+            // program next refresh
+            setTimeout(update, delay);
+          })
+          .attr('href', uri.replace(/\?/, '?'+ parameter + '=' + refreshed));
+      };
+  
+      setTimeout(update, delay);
+    }
+
+  });
+
+}(jQuery));
